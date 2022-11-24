089230e1e327ff34e8914f789e8c66186f18f118
Jaydin Andrews <jaydin.andrews@gmail.com>
2022-11-24 18:37:46 -0500

    start asm tests

--- test/test_asm.py
+++ test/test_asm.py
@@ -1,9 +1,17 @@
+import sys
 import unittest
+from unittest.mock import patch
 
 from .context import asm
 
 class AssemblerTestSuite(unittest.TestCase):
     """Assembler test cases."""
+
+    def test_parse_args(self):
+        testargs = ["core", "-o", "test/bin/fff.OUT", "./prog/basicAdd.asm"]
+
+        with unittest.mock.patch('sys.argv', testargs):
+            assembler = asm.Assembler()
 
 if __name__ == '__main__':
     assembler = asm.Assembler()
