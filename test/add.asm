528e183d551669524987af69d10874c1e426a805
Jaydin Andrews <jaydin.andrews@gmail.com>
2022-11-24 18:37:44 -0500

    update test add

--- test/add.asm
+++ test/add.asm
@@ -1,8 +1,8 @@
-MAIN: mv #3, r0
-      mv #5, r1
-LOOP: inc r1
-      dec r0
-      cmp #0, r0
+MAIN: mv #3, a
+      mv #5, b
+LOOP: inc b
+      dec a
+      cmp #0, a
       jnz LOOP    ;this is a comment
       hlt
    ;This is also comment
