4610b07dd3293c6413701b005b145cba718d010a
Jaydin Andrews <jaydin.andrews@gmail.com>
2022-11-24 18:37:46 -0500

    refactor emu

--- emu/core.py
+++ emu/core.py
@@ -1,31 +1,53 @@
-import argparse
-from ctypes import c_uint16
-from reg import Registers, Flags
-from array import array
+import argparse, array
+from reg import Registers
 from pathlib import Path
 from struct import unpack
 
 
 class Emulator(object):
+    debug_mode = False
+
     def __init__(self):
         description = "LLAMA-16 Emulator"
         parser = argparse.ArgumentParser(description=description)
         parser.add_argument("program",
                             default="",
                             help="input program to be ran")
+        parser.add_argument("-d",
+                            "--debug",
+                            action="store_true",
+                            help="print extra debugging infomation")
         args = parser.parse_args()
+        if args.debug:
+            self.debug_mode = True
 
-        self.memory = array('H', [0]*65536)
+        self.memory = array.array('H', [0]*65536)
         self.registers = Registers()
-        self.registers.pc.value = 0x4000
         self.load_program(args.program)
 
+        self.dump_state()
+
+    def dump_state(self):
+        print("======== LLAMA-16 Emulator State ========")
+        print(f"IP: {hex(self.registers.reg_read('ip'))}")
+        print(f"SP: {hex(self.registers.reg_read('sp'))}")
+        print(f"BP: {hex(self.registers.reg_read('bp'))}")
+        print(f"F:  {self.registers.get_flags()}")
+
+    def mem_read(address):
+        return self.memory[address]
+
+    def mem_write(address, value):
+        self.memory[address] = value
+   
     def load_program(self, filename):
         with open(filename, 'rb') as f:
             c = f.read()
+            if debug_mode:
+                print(f"loading {filename} of size {len(c)} bytes")
         for count in range(0, len(c), 2):
-            self.memory[int(0x4000+count/2)] = unpack('>H', c[count:count+2])[0]
-            
+            self.memory[int(0x4000+count/2)] = unpack('<H', c[count:count+2])[0]
 
+   
 if __name__ == "__main__":
     emulator = Emulator()
--- emu/reg.py
+++ emu/reg.py
@@ -1,19 +1,69 @@
 from ctypes import c_uint16
 from array import array
-from enum import IntEnum
 
 class Registers():
     def __init__(self):
-        self.gprs = array('H', [0]*4)
-        self.pc = (c_uint16)()
-        self.sp = (c_uint16)()
-        self.bp = (c_uint16)()
-        self.flags = (c_uint16)()
+        # 0-3 : A-D, 4: IP, 5: SP, 6: BP, 7: F
+        self.gprs = array('H', [0]*8)
+        # Set IP, SP, BP, and F
+        self.gprs[4] = 0x4000
+        self.gprs[5] = 0xFFBF
+        self.gprs[6] = 0xFFBF
 
-class Flags(IntEnum):
-    positive = 0
-    zero = 1
-    negative = 2
-    less = 3
-    equal = 4
-    great = 5
+        self.registers = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'ip': 4, 'sp': 5, 'bp': 6, 'f': 7}
+        self.flags = {'halt': 6, 'greater': 5, 'equal': 4, 'less': 3, 'negative': 2, 'zero': 1, 'positive': 0}
+
+    def reg_write(self, register, value):
+        self.gprs[self.registers[register]] = value
+
+    def reg_read(self, register):
+        return self.gprs[self.registers[register]]
+
+    def update_flags(self, register, set_negative=False):
+        # Clear bits 0-2
+        self.reg_write('f', self.reg_read('f') & 0xFFF8)
+        if set_negative == True:
+            self.reg_write('f', 1 << 2)
+            return
+
+        if self.reg_read(register) == 0:
+            self.reg_write('f', 1 << 1)
+        elif self.reg_read(register) > 0:
+            self.reg_write('f', 1 << 0)
+
+    def update_cmp_flags(self, a, b):
+        # Clear bits 3-5
+        self.reg_write('f', self.reg_read('f') & 0xFFC7)
+        if a > b:
+            self.reg_write('f', 1 << 5)
+        elif b < a:
+            self.reg_write('f', 1 < 3)
+        elif a == b:
+            self.reg_write('f', 1 << 4)
+
+    def clear_flags(self):
+        self.reg_write('f', 0x0)
+
+    def get_flags(self):
+        # 0000 0000 0HGE LNZP
+        set_flags = []
+        cmp_flags = self.reg_read('f') & 0x0038
+        if cmp_flags == 32:
+            set_flags.append('greater')
+        elif cmp_flags == 16:
+            set_flags.append('equal')
+        elif cmp_flags == 8:
+            set_flags.append('less')
+
+        flags = self.reg_read('f') & 0x0007 
+        if flags == 4:
+            set_flags.append('negative')
+        elif flags == 2:
+            set_flags.append('zero')
+        elif flags == 1:
+            set_flags.append('positive')
+
+        return set_flags
+
+    def set_halt(self):
+        self.reg_write('f', 1 << 6)
